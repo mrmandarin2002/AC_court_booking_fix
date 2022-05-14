84e0db36475e7397fd8329bccc29eaf8080d3d60
Derek Ma <46353341+mrmandarin2002@users.noreply.github.com>
2022-05-13 23:57:37 -0400

    Revert "minor style stuff & python-poetry virtualenv"
    This reverts commit ebc4c88a2b85cdbe6c53d9a7c5e71452d280e150.

--- CONSTS.py
+++ CONSTS.py
@@ -1,4 +1,2 @@
 # links for each court
-COURTS = ["https://recreation.utoronto.ca/booking/af97abfd-f094-49c4-8a17-9330879ed6cc",
-          "https://recreation.utoronto.ca/booking/08ec6e7c-068d-4875-94a2-178ddef80b9b",
-          "https://recreation.utoronto.ca/booking/c8f30b5c-8c2c-4d9e-bb7c-bb214bc94c7d"]
+COURTS = ["https://recreation.utoronto.ca/booking/af97abfd-f094-49c4-8a17-9330879ed6cc", "https://recreation.utoronto.ca/booking/08ec6e7c-068d-4875-94a2-178ddef80b9b", "https://recreation.utoronto.ca/booking/c8f30b5c-8c2c-4d9e-bb7c-bb214bc94c7d"]
