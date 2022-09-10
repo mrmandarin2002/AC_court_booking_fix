ef4917231e1b02951974c1112b608c72eee76efe
Derek Ma <46353341+mrmandarin2002@users.noreply.github.com>
2022-09-10 15:22:01 -0400

    blah blah

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 # fuck AC
-import time
+from re import T
+import time, os
 
 from multiprocessing import Process
 
@@ -16,15 +17,18 @@ from selenium.webdriver.firefox.options import Options
 options = Options()
 options.headless = True
 
-profile = webdriver.FirefoxProfile()
-profile.set_preference('permissions.default.image', 2)
-
 from CONSTS import *
 from SECRETS import *
 
+waitTime = 30
+
+os.environ['GH_TOKEN'] = "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"
+
+skips = [[] for i in range(10)]
+
 class Bot:
 
-    def __init__(self, court : int, booking_times : list, back, skip : int = 0):
+    def __init__(self, court : int, booking_times : list, back, id : int, skip : int = 0):
         self.skip = skip
         self.back = back
         self.court = court
@@ -35,87 +39,80 @@ class Bot:
 
     # where the magic happens
     def start(self):
-        self.driver = webdriver.Firefox(firefox_profile = profile, service=Service(GeckoDriverManager().install()), options = options)
+        self.driver = webdriver.Firefox(service=Service(GeckoDriverManager().install()), options = options)
         self.driver.get(COURTS[self.court])
-
-        time.sleep(0.5)
         # presses "login with utorid button"
-        WebDriverWait(self.driver, 10).until(
-            EC.visibility_of_element_located((By.XPATH, "//button[@class='btn btn-primary btn-block btn-external-login btn-sign-in btn-sso-shibboleth']"))
+        time.sleep(1)
+        WebDriverWait(self.driver, waitTime).until(
+            EC.presence_of_element_located((By.XPATH, "//button[@class='btn btn-primary btn-block btn-external-login btn-sign-in btn-sso-shibboleth']"))
         ).click()
 
         # fill username
-        WebDriverWait(self.driver, 10).until(
+        WebDriverWait(self.driver, waitTime).until(
             EC.presence_of_element_located((By.ID, "username"))
         ).send_keys(UTORID)
 
         # fill password
-        WebDriverWait(self.driver, 10).until(
+        WebDriverWait(self.driver, waitTime).until(
             EC.presence_of_element_located((By.ID, "password"))
         ).send_keys(PASSWORD)
 
         # click submit
-        WebDriverWait(self.driver, 10).until(
+        WebDriverWait(self.driver, waitTime).until(
             EC.presence_of_element_located((By.XPATH, "//button[@class='btn btn-primary btn-lg']"))
         ).click()
-        self.book()
 
-
-    def book(self):
         # accept the fucking cookies
-        WebDriverWait(self.driver, 10).until(
+        WebDriverWait(self.driver, waitTime).until(
             EC.presence_of_element_located((By.ID, 'gdpr-cookie-accept'))
         ).click()
-        
-        date = '0'
-        # keep on looping, we try clicking every book button
-        while True:
+        self.date = '0'
+        self.book()
 
-            startTime = time.time()
-            #time.sleep(0.4)
-            #date_button = self.driver.find_element_by_xpath("//button[@class='btn btn-secondary single-date-select-button single-date-select-one-click'][2]")
-            date_button = WebDriverWait(self.driver, 10).until(
-                EC.presence_of_element_located((By.XPATH, "//button[@class='btn btn-secondary single-date-select-button single-date-select-one-click'][2]"))
-            )
-            # print("Presence Time: ", time.time() - startTime) 
+    def book(self):     
+        while True: 
+            try:
+                # keep on looping, we try clicking every book button
+                startTime = time.time()
+                date_button = WebDriverWait(self.driver, 10).until(
+                    EC.presence_of_element_located((By.XPATH, "//button[@class='btn btn-secondary single-date-select-button single-date-select-one-click'][2]"))
+                )
+                # print("Presence Time: ", time.time() - startTime) 
 
-            new_date = date_button.get_attribute("data-day")
-            #print(new_date)
-            if date != new_date:
-                print("IN NEW DATE!")
-                date_button.click()
-                date = new_date
-                book_buttons = [0]
-                
-                while len(book_buttons):
-                    book_buttons = self.driver.find_elements(By.XPATH, "//button[@class='btn btn-primary']")
-                    del book_buttons[0]
-                    if self.back:
-                        book_buttons.reverse()
+                new_date = date_button.get_attribute("data-day")
+                #print(new_date)
+                if self.date != new_date:
+                    print("IN NEW DATE!")
+                    date_button.click()
+                    self.date = new_date
+                    book_buttons = [0]
+                    
+                    while len(book_buttons):
+                        book_buttons = self.driver.find_elements(By.XPATH, "//button[@class='btn btn-primary']")
+                        del book_buttons[0]
 
-                    try:
                         if book_buttons:
+                            if self.back:
+                                book_buttons.reverse()
                             book_buttons[min(self.skip, len(book_buttons) - 1)].click()
                             print("BOOKED!")
-                    except:
-                        print("OOOPSSS")
+                            time.sleep(0.5)
 
-            
-            self.driver.get(COURTS[self.court])
-            # print("Time taken:", time.time() - startTime)
-            # print("------------")
+                self.driver.get(COURTS[self.court]) 
+                time.sleep(0.2)
+            except:
+                pass
 
+court = 1
 
 
 if __name__ == '__main__':
     bots = []
-    for i in range(4):
-        bots.append(Bot(1, [], skip = i, back = True))
-        bots.append(Bot(1, [], skip = i, back = True))
-        bots.append(Bot(1, [], skip = i, back = True))
-        bots.append(Bot(1, [], skip = i, back = True))
-
-#bots = [Bot(i, []) for i in range(3)]
+    for i in range(3):
+        for x in range(6):
+            bots.append(Bot(i, [], id = x, skip = 1, back = True))
+            time.sleep(0.3)
+    # testBot = Bot(0, [], id = 0, skip = 0, back = True)
 
 
 
